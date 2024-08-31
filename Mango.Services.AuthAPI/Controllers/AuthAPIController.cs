4be2876fca1e910ffc44f0b9b87f79e5a8975366
Balaji Krishnakumar <balajisathesh.bs@gmail.com>
2024-08-31 18:23:02 +0530

    added controller

--- Mango.Services.AuthAPI/Controllers/AuthAPIController.cs
+++ Mango.Services.AuthAPI/Controllers/AuthAPIController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Mango.Services.AuthAPI.Models.Dto;
+using Mango.Services.AuthAPI.Service.IService;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Mango.Services.AuthAPI.Controllers
@@ -7,16 +9,45 @@ namespace Mango.Services.AuthAPI.Controllers
     [ApiController]
     public class AuthAPIController : ControllerBase
     {
-        [HttpPost("register")]
-        public async Task<IActionResult> Register()
+        private readonly IAuthService _authService;
+        protected ResponseDto _responseDto;
+
+
+        public AuthAPIController(IAuthService authService)
         {
-            return Ok();
+            _authService = authService;
+            _responseDto = new();
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] RegistrationRequestDto registrationRequest)
+        {
+            var errorMessage = await _authService.Register(registrationRequest);
+
+            if (!string.IsNullOrEmpty(errorMessage)) 
+            { 
+                _responseDto.IsSuccess = false;
+                _responseDto.Message = errorMessage;
+                return BadRequest(_responseDto);
+            }
+
+            return Ok(_responseDto);
         }
 
         [HttpPost("login")]
-        public async Task<IActionResult> Login()
+        public async Task<IActionResult> Login([FromBody] LoginRequestDto loginRequest)
         {
-            return Ok();
+            var loginResponse = await _authService.Login(loginRequest);
+
+            if (loginResponse.User == null)
+            {
+                _responseDto.IsSuccess = false;
+                _responseDto.Message = "Username or Password is Incorrect";
+                return BadRequest(_responseDto);
+            }
+
+            _responseDto.Result = loginResponse;
+            return Ok(_responseDto);
         }
     }
 }
