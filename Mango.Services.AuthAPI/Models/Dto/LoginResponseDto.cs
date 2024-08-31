a5be4ac186c5378c3a03db7b5d9d66b6aa21c836
Balaji Krishnakumar <balajisathesh.bs@gmail.com>
2024-08-31 18:22:45 +0530

    added models

---
+++ Mango.Services.AuthAPI/Models/Dto/LoginResponseDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Mango.Services.AuthAPI.Models.Dto
+{
+    public class LoginResponseDto
+    {
+        public UserDto User { get; set; } = new();
+        public string Token { get; set; } = string.Empty;
+    }
+}
