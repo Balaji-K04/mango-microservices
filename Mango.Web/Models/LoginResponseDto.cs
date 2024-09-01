4c1316d338b90a34e675c66d589bf30e431d45d3
Balaji Krishnakumar <balajisathesh.bs@gmail.com>
2024-09-01 07:39:00 +0530

    Added Dtos

---
+++ Mango.Web/Models/LoginResponseDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Mango.Web.Models
+{
+    public class LoginResponseDto
+    {
+        public UserDto User { get; set; } = new();
+        public string Token { get; set; } = string.Empty;
+    }
+}
